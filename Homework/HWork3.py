4430f6b93bcbd5c6d19f763b914602e84bd84678
KatrinBlade <126530739+KatrinBladePT@users.noreply.github.com>
2023-06-02 09:48:14 +0300

    exercise16

--- Homework/HWork3.py
+++ Homework/HWork3.py
@@ -2,4 +2,12 @@
 # A[1..N]. Пользователь в первой строке вводит натуральное число N – количество 
 # элементов в массиве. В последующих  строках записаны N целых чисел Ai. Последняя 
 # строка содержит число X
+n = int(input("Enter list size: "))
+print("size:", n)
+a = list(map(int, input("Enter list: ").split()))
+print("list:", a)
+x = int(input("Enter number: "))
+print("number:", x)
+count = a.count(x)
 
+print(count)
