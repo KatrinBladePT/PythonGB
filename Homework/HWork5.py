0d1f6716721c608ec8085fad4575ee1b565e418a
KatrinBlade <126530739+KatrinBladePT@users.noreply.github.com>
2023-06-14 23:46:26 +0300

    exercise28

--- Homework/HWork5.py
+++ Homework/HWork5.py
@@ -13,3 +13,19 @@ a = int(input("Enter number A: "))
 b = int(input("Enter number B: "))
 result = power(a, b)
 print(result)
+
+# Задача 28: Напишите рекурсивную функцию sum(a, b), возвращающую сумму двух целых 
+# неотрицательных чисел. Из всех арифметических операций допускаются только +1 и -1. 
+# Также нельзя использовать циклы.
+
+def sum(a, b):
+    if a == 0:
+        return b
+    elif b == 0:
+        return a
+    else:
+        return sum(a-1, b) + 1 if a > b else sum(a, b-1) + 1
+a = int(input("Enter number A: ")) 
+b = int(input("Enter number B: ")) 
+print(sum(a,b)) 
+   
