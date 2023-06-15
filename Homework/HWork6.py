87304ea946fa0d46491767d82f29785b2464e431
KatrinBlade <126530739+KatrinBladePT@users.noreply.github.com>
2023-06-15 04:30:53 +0300

    exercise32

--- Homework/HWork6.py
+++ Homework/HWork6.py
@@ -14,3 +14,21 @@ for i in range(n):
 
 print(progression) 
 
+# Задача 32: Определить индексы элементов массива (списка), значения 
+# которых принадлежат заданному диапазону (т.е. не меньше заданного минимума 
+# и не больше заданного максимума)
+
+def find_indexes(lst, min_val, max_val):
+    result = []
+    for i in range(len(lst)):
+        if min_val <= lst[i] <= max_val:
+            result.append(i)
+    return result
+
+lst = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+min_val = 3
+max_val = 7
+indexes = find_indexes(lst, min_val, max_val)
+print(indexes)
+
+
