f70ac0d994d0e66e0fb4b6c3005b3562eab03300
KatrinBlade <126530739+KatrinBladePT@users.noreply.github.com>
2023-06-15 04:42:30 +0300

    exercise32_2

--- Homework/HWork6.py
+++ Homework/HWork6.py
@@ -31,4 +31,15 @@ max_val = 7
 indexes = find_indexes(lst, min_val, max_val)
 print(indexes)
 
+# another example
+
+def find_indexes2(lst2, min_val2, max_val2):
+    return [i for i, x in enumerate(lst2) if min_val2 <= x <= max_val2]
+
+lst2 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+min_val2 = 3
+max_val2 = 7
+indexes2 = find_indexes2(lst2, min_val2, max_val2)
+print(indexes2)
+
 
