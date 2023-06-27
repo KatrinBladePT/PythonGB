b448c7dd74bec9edacb484feb173b952e6b350c6
KatrinBlade <126530739+KatrinBladePT@users.noreply.github.com>
2023-06-27 05:31:08 +0300

    addcalifhousingcomplite

--- Homework/HWork9/HWork9.py
+++ Homework/HWork9/HWork9.py
@@ -1,4 +1,13 @@
-import pandas as pd
+# Задача 40: Работать с файлом california_housing_train.csv, который 
+# находится в папке sample_data. Определить среднюю стоимость дома, где 
+# кол-во людей от 0 до 500 (population).
 
-DataFrame.head(n=5)
+filtered_data = data[data["population"] < 501]
+print(data["median_house_value"].mean())
 
+# Задача 42: Узнать какая максимальная households в зоне минимального значения population.
+
+min_population = data["population"].min()
+min_population_data = data[data["population"] == min_population]
+max_households = min_population_data["households"].max()
+print(max_households)
